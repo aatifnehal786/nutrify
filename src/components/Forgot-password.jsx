e5d0ad2aa2602f08b5b164f003bb249dc48c3799
aatifnehal786 <aatifnehal786@gmail.com>
2025-07-29 17:11:21 +0530

    .

--- src/components/Forgot-password.jsx
+++ src/components/Forgot-password.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react"
+import { useState,useEffect } from "react"
 import { Link } from "react-router-dom";
 import show from '../assets/show.png'
 import hide from '../assets/hide.png'
